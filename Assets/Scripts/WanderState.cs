bc3d1058253f8ff83554fb901b0fd7b619baf079
aks024680 <130197640+aks024680@users.noreply.github.com>
2023-08-17 19:36:19 +0800

    Update enemywalkaround

--- Assets/Scripts/WanderState.cs
+++ Assets/Scripts/WanderState.cs
@@ -13,6 +13,17 @@ namespace TSU.TwoD
         private float OffsetRight = 2;
         [SerializeField,Header("移動速度"),Range(0,5)]
         private float speed = 1.5f;
+        [SerializeField, Header("待機狀態")]
+        private WanderState stateIdle;
+        [SerializeField, Header("是否開始待機")]
+        private bool startIdle;
+        [SerializeField, Header("遊走狀態的隨機範圍")]
+        private Vector2 rangeWanderTime = new Vector2(0, 3);
+
+        private float timeWander;
+
+        private float timer;
+
         private Vector3 pointLeft => pointOriginal + Vector3.right * OffsetLeft;
         private Vector3 pointRight => pointOriginal + Vector3.right * OffsetRight;
         /// <summary>
@@ -24,6 +35,7 @@ namespace TSU.TwoD
         private Rigidbody2D rb;
         private void Start()
         {
+            timeWander = Random.Range(rangeWanderTime.x, rangeWanderTime.y);
             anim = GetComponent<Animator>();
             rb = GetComponent<Rigidbody2D>();
         }
@@ -36,21 +48,36 @@ namespace TSU.TwoD
         }
         public override State RunCurrentState()
         {
-            if (Vector3.Distance(transform.position, pointRight) < 0.1f)
+
+          
+
+            if (Vector3.Distance(transform.position, pointRight) < 1f)
             {
                 direction = -1;
                 transform.eulerAngles = new Vector3(0, 180, 0);
             }
-            if (Vector3.Distance(transform.position, pointLeft) < 0.1f)
+            if (Vector3.Distance(transform.position, pointLeft) < 1f)
             {
                 direction = 1;
                 transform.eulerAngles = Vector3.zero;
             }
-            
-            
+
+
             rb.velocity = new Vector2(direction * speed, rb.velocity.y);
             anim.SetBool(parWander, true);
-            return this;
+            
+
+            timer += Time.deltaTime;
+            print($"<color=blue>計時器 : {timer}</color>");
+            if (timer >= timeWander) startIdle = true;
+            if (startIdle)
+            {
+                return stateIdle;
+            }
+            else
+            {
+                return this;
+            }
         }
         [ContextMenu("取得腳色原始座標")]
         private void GetOriginalPoint() { 
